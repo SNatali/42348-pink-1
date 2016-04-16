1b22fcc193cabb96c66690f248417ccfe6511423
Nikolay Gromov <nicothin@gmail.com>
2016-04-16 17:49:43 +0300

    new

--- js/script.js
+++
@@ -1,6 +0,0 @@
-var toggler = document.getElementById('close-form');
-toggler.onclick = function(e){
-  e.preventDefault();
-  toggler.classList.toggle('close-form--close');
-  document.getElementById('main-nav__list').classList.toggle('main-nav__list--visible');
-}
