94838aca1a5967696879f64c0d45f23bbdb042d2
Natalia Sergieieva <bossovivh@mail.ru>
2016-06-26 22:41:42 +0200

    style

--- src/blocks/editor-photo/editor-photo.js
+++ src/blocks/editor-photo/editor-photo.js
@@ -1,3 +1,8 @@
 // (function(){
 // код
 // }());
+
+$('.editor-photo__item').on('click', function(){
+  $('.editor-photo__item').removeClass('editor-photo__item--active');
+  $(this).addClass('editor-photo__item--active');
+});
